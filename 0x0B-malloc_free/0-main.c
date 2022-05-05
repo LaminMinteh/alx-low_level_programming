af706195c5fd26b1ac0b1eb9a6bc21064738db95
LaminMinteh <mintehl6@gmail.com>
2022-05-05 15:39:32 +0000

    0-create_array.c

--- 0x0B-malloc_free/0-main.c
+++ 0x0B-malloc_free/0-main.c
@@ -9,7 +9,6 @@
  *
  * Return: Nothing.
  */
-
 void simple_print_buffer(char *buffer, unsigned int size)
 {
 unsigned int i;
@@ -22,7 +21,7 @@ if (i % 10)
 printf(" ");
 }
 if (!(i % 10) && i)
-}
+{
 printf("\n");
 }
 printf("0x%02x", buffer[i]);
