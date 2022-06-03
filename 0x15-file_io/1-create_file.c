66eaf12952cc3eeb7c8a66e71249e3121d52065a
LaminMinteh <mintehl6@gmail.com>
2022-06-03 12:21:23 +0000

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,30 +4,27 @@
  * @filename: filename.
  * @text_content: content writed in the file.
  *
- * return: 1 if sucessful.
+ * Return: If the function fails - -1.
+ * Otherwise - 1.
  */
 
 int create_file(const char *filename, char *text_content)
 {
 
-int f;
-int letter;
-int rwr;
-
-if (!filename)
+int op, wr, len = 0;
+if (filename == NULL)
 return (-1);
+if (text_content != NULL)
+{
+for (len = 0; text_content[len];)
+len++;
+}
 
-f = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-if (f == -1)
+op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+wr = write(op, text_content, len);
+
+if (op == -1 || wr == -1)
 return (-1);
-
-if (!text_content)
-text_content = "";
-
-for (letter = 0; text_content[letter]; letter++)
-rwr = write(f, text_content, letter);
-if (rwr == -1)
-return (-1);
-close(f);
+close(op);
 return (1);
 }
