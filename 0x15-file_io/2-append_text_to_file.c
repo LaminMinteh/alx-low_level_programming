07c348af206c832b82105aa616efaf3de1472a99
LaminMinteh <mintehl6@gmail.com>
2022-06-03 12:45:07 +0000

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,28 +10,23 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
+int op, wr, len = 0;
 
-int fd;
-int nletters;
-int rwr;
-
-if (!filename)
+if (filename == NULL)
 return (-1);
 
-fd = open(filename, O_WRONLY | O_APPEND);
-if (fd == -1)
-return (-1);
-
-if (text_content)
-
+if (text_content != NULL)
 {
-for (nletters = 0; text_content[nletters]; nletters++);
-rwr = write(fd, text_content, nletters);
-if (rwr == -1)
-return (-1);
+for (len = 0; text_content[len];)
+len++;
 }
 
-close(fd);
+op = open(filename, O_WRONLY | O_APPEND);
+wr = write(op, text_content, len);
+
+if (op == -1 || wr == -1)
+return (-1);
+
+close(op);
 return (1);
 }
-
