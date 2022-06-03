010b2bb79cd530d3b427dcada979d0b2f907c2ba
LaminMinteh <mintehl6@gmail.com>
2022-06-03 12:34:55 +0000

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -17,10 +17,13 @@ int rwr;
 
 if (!filename)
 return (-1);
+
 fd = open(filename, O_WRONLY | O_APPEND);
 if (fd == -1)
 return (-1);
+
 if (text_content)
+
 {
 for (nletters = 0; text_content[nletters]; nletters++);
 rwr = write(fd, text_content, nletters);
@@ -31,3 +34,4 @@ return (-1);
 close(fd);
 return (1);
 }
+
